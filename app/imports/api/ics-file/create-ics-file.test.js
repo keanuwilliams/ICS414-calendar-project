ae1ba8fd501099841302e906649ed6cae80b2a81
Keanu Williams <keanucwilliams@gmail.com>
2020-07-06 23:28:51 -1000

    Fixed ESLint errors and cleaned up the Landing page code

--- app/imports/api/ics-file/create-ics-file.test.js
+++ app/imports/api/ics-file/create-ics-file.test.js
@@ -1,33 +1,35 @@
+import saveAs from 'file-saver';
+
 /**
- * Creates the sample .ics file
+ * Creates a sample .ics file
  * @author Keanu Williams
  */
 
-import saveAs from 'file-saver';
-
-function icsFile () {
+export function createICSFile() {
 
     // The beginning of the file
     let file =
-        'BEGIN:VCALENDAR\n'+
-        'PRODID:-//Apple Inc.//Mac OS X 10.15.5//EN\n'+
-        'VERSION:2.0\n'+
+        'BEGIN:VCALENDAR\n' +
+        'PRODID:-//Apple Inc.//Mac OS X 10.15.5//EN\n' +
+        'VERSION:2.0\n' +
         'CALSCALE:GREGORIAN\n';
 
     // Add sample event
     file +=
-        'BEGIN:VEVENT\n'+
-        'SUMMARY:ICS414 Meeting #2\n'+
-        'DESCRIPTION:Meet up with Nancy to discuss the progress we are making with our project.\n'+
-        'DTSTAMP:20200701T002526Z\n'+
-        'DTSTART:20200701T120000Z\n'+
-        'DTEND:20200701T121000Z\n'+
+        'BEGIN:VEVENT\n' +
+        'SUMMARY:ICS414 Meeting #2\n' +
+        'DESCRIPTION:Meet up with Nancy to discuss the progress we are making with our project.\n' +
+        'DTSTAMP:20200701T002526Z\n' +
+        'DTSTART:20200701T120000Z\n' +
+        'DTEND:20200701T121000Z\n' +
         'END:VEVENT\n';
 
     // End .ics file
     file += 'END:VCALENDAR\n';
 
-    let blob = new Blob([file]);
-    saveAs(blob, "example.ics");
+    // The comment below used to suppress Blob being undefined
+    // eslint-disable-next-line no-undef
+    const blob = new Blob([file]);
+    saveAs(blob, 'example.ics');
     return file;
 }
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,39 +1,9 @@
 import React from 'react';
-import {Grid, Image, Button} from 'semantic-ui-react';
-import '../../../imports/api/ics-file/create-ics-file.test';
+import { Grid, Image, Button } from 'semantic-ui-react';
+import { createICSFile } from '../../api/ics-file/create-ics-file.test';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    downloadICSFile = () => {
-        // The beginning of the file
-        let file =
-            'BEGIN:VCALENDAR\n'+
-            'PRODID:-//Apple Inc.//Mac OS X 10.15.5//EN\n'+
-            'VERSION:2.0\n'+
-            'CALSCALE:GREGORIAN\n';
-
-        // Add sample event
-        file +=
-            'BEGIN:VEVENT\n'+
-            'SUMMARY:ICS414 Meeting #2\n'+
-            'DESCRIPTION:Meet up with Nancy to discuss the progress we are making with our project.\n'+
-            'DTSTAMP;TZID=Pacific/Honolulu;VALUE=DATE-TIME:20200701T002526Z\n'+
-            'DTSTART;TZID=Pacific/Honolulu;VALUE=DATE-TIME:20200701T120000Z\n'+
-            'DTEND;TZID=Pacific/Honolulu;VALUE=DATE-TIME:20200701T121000Z\n'+
-            'END:VEVENT\n';
-
-        // End .ics file
-        file += 'END:VCALENDAR\n';
-
-        let blob = new Blob([file]);
-        saveAs(blob, "example.ics");
-        return file;
-    };
 
     render() {
         return (
@@ -44,9 +14,9 @@ class Landing extends React.Component {
                 </Grid.Column>
 
                 <Grid.Column width={8}>
-                    <h1>Team The Goblet's Calendar Template!</h1>
+                    <h1>Team The Goblet&apos;s Calendar Template!</h1>
                     <p>Click down below to download our sample .ics file.</p>
-                    <Button content="Download" onClick={this.downloadICSFile}/>
+                    <Button content="Download" onClick={createICSFile} />
                 </Grid.Column>
 
             </Grid>
