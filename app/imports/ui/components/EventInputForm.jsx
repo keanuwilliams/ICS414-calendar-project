fadf40227d46946f8b762240b124e13a79842e42
Keanu Williams <keanucwilliams@gmail.com>
2020-07-15 00:15:19 -1000

    Changed the title on page and navbar. Also, began the implementation of the event input.

--- app/imports/ui/components/EventInputForm.jsx
+++ app/imports/ui/components/EventInputForm.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Button, Form, Segment, Header } from 'semantic-ui-react';
 
 class EventInputForm extends React.Component {
     render() {
         return (
-            <p>test</p>
+            <Form>
+                <Segment>
+                    <Header as='h3'>Add Event</Header>
+                    <Form.Input
+                        required
+                        label='Event Name'
+                        placeholder='Event Name'
+                    />
+                    <Form.Input required label='Start Date'>
+                        
+                    </Form.Input>
+                    <Button type='submit' content='Add Event' />
+                </Segment>
+            </Form>
         );
     }
 }
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import { Grid, Button } from 'semantic-ui-react';
+import Planner from '../components/Planner';
+import EventInputForm from '../components/EventInputForm';
 import { createSample } from '../../api/ics-file/create-ics-file';
-import Calendar from 'react-calendar';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
 
-  render() {
-    return (
-      <Grid verticalAlign='middle' textAlign='center' container>
-        <Grid.Column width={8}>
-          <h1>Team The Goblet&apos;s Calendar Template!</h1>
-          <p>Click down below to download our sample .ics file.</p>
-          <Button content="Download" onClick={createSample} />
-        </Grid.Column>
-
-        <Grid.Column width={6}>
-          <Calendar />
-        </Grid.Column>
-
-
-      </Grid>
-    );
-  }
+    render() {
+        return (
+            <Grid verticalAlign='middle' textAlign='center' container>
+                <Grid.Row>
+                    <Grid.Column width={8}>
+                        <h1>TheGoblet&apos;s Event Planner!</h1>
+                        <p>Click below to download our sample .ics file.</p>
+                        <Button content="Download" onClick={ createSample } />
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row>
+                    <Grid.Column width={10}>
+                        <Planner />
+                    </Grid.Column>
+                    <Grid.Column textAlign='left' width={4}>
+                        <EventInputForm />
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        );
+    }
 }
 
 export default Landing;
