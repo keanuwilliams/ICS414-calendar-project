f7307dfebfadc1d3c7db7dfe184f6fe5944df63f
Ryota Seki <ryotabs@hawaii.edu>
2020-07-19 21:21:01 -1000

    Change form to use uniforms library and change formatting to use 'prettier'

--- app/imports/ui/components/EventInputForm.jsx
+++ app/imports/ui/components/EventInputForm.jsx
@@ -1,105 +1,50 @@
-import React from 'react';
-import { Button, Form, Segment, Checkbox } from 'semantic-ui-react';
-import DatePicker from 'react-datepicker';
+import React from "react";
+import {
+  Button,
+  Form,
+  Segment,
+  Checkbox,
+  Grid,
+  Header,
+} from "semantic-ui-react";
+import {
+  AutoForm,
+  TextField,
+  SelectField,
+  SubmitField,
+} from "uniforms-semantic";
+import DatePicker from "react-datepicker";
+import SimpleSchema from "simpl-schema";
+
+const eventFormSchema = new SimpleSchema({
+  eventName: String,
+  date: Date,
+});
 
 class EventInputForm extends React.Component {
-
-    state = {
-        startDate: new Date(),
-        endDate: new Date(),
-        allDay: false,
-    };
-
-    handleStartDate = date => {
-        if (date <= this.state.endDate) {
-            this.setState({
-                startDate: date,
-            });
-        }
-    };
-
-    handleEndDate = date => {
-        if (date >= this.state.startDate) {
-            this.setState({
-                endDate: date,
-            });
-        }
-    };
-
-    handleAllDay = () => this.setState((prevState) => ({ allDay: !prevState.allDay }));
-
-    render() {
-        return (
-            <Form>
-                <Segment>
-                    <Form.Input
-                        required
-                        placeholder='Event Name'
-                    />
-                    {this.state.allDay === false ? (
-                        <Form.Group>
-                            <Form.Input required label='Start Date'>
-                                <DatePicker
-                                    required
-                                    selectsStart
-                                    showTimeSelectOnly
-                                    timeFormat='HH:mm'
-                                    dateFormat='MM/d/yyyy h:mm aa'
-                                    selected={this.state.startDate}
-                                    onChange={this.handleStartDate}
-                                    startDate={this.state.startDate}
-                                    endDate={this.state.endDate}
-                                />
-                            </Form.Input>
-                            <Form.Input required label='End Date'>
-                                <DatePicker
-                                    required
-                                    selectsEnd
-                                    showTimeSelectOnly
-                                    timeFormat='HH:mm'
-                                    dateFormat='MM/d/yyyy h:mm aa'
-                                    selected={this.state.endDate}
-                                    onChange={this.handleEndDate}
-                                    startDate={this.state.startDate}
-                                    endDate={this.state.endDate}
-                                />
-                            </Form.Input>
-                        </Form.Group>
-                    ) : (
-                            <Form.Group>
-                                <Form.Input required label='Start Date'>
-                                    <DatePicker
-                                        required
-                                        selectsStart
-                                        showTimeSelectOnly
-                                        selected={this.state.startDate}
-                                        onChange={this.handleStartDate}
-                                        startDate={this.state.startDate}
-                                        endDate={this.state.endDate}
-                                    />
-                                </Form.Input>
-                                <Form.Input required label='End Date'>
-                                    <DatePicker
-                                        required
-                                        selectsEnd
-                                        showTimeSelectOnly
-                                        selected={this.state.endDate}
-                                        onChange={this.handleEndDate}
-                                        startDate={this.state.startDate}
-                                        endDate={this.state.endDate}
-                                    />
-                                </Form.Input>
-                            </Form.Group>
-                        )}
-                    <Form.Field control={Checkbox}
-                        label='All Day'
-                        onChange={this.handleAllDay}
-                    />
-                    <Button secondary type='submit' content='Add Event' />
-                </Segment>
-            </Form>
-        );
-    }
+  render() {
+    let fRef = null;
+    return (
+      <Grid container centered>
+        <Grid.Column>
+          <Header as="h3" textAlign="center">
+            Create Event
+          </Header>
+          <AutoForm
+            ref={(ref) => {
+              fRef = ref;
+            }}
+            schema={eventFormSchema}
+            onSubmit={(data) => this.submit(data, fRef)}
+          >
+            <Segment>
+              <TextField name="eventName" />
+            </Segment>
+          </AutoForm>
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
 export default EventInputForm;
--- app/imports/ui/components/EventInputForm.test.jsx
+++ app/imports/ui/components/EventInputForm.test.jsx
@@ -1,20 +1,19 @@
-import { Meteor } from 'meteor/meteor';
-import React, { createFactory } from 'react';
-import { configure, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { expect } from 'chai';
-import EventInputForm from './EventInputForm';
-import { resetDatabase } from 'meteor/xolvio:cleaner';
-import { Factory } from 'meteor/dburles:factory';
+import { Meteor } from "meteor/meteor";
+import React, { createFactory } from "react";
+import { configure, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { expect } from "chai";
+import EventInputForm from "./EventInputForm";
+import { resetDatabase } from "meteor/xolvio:cleaner";
+import { Factory } from "meteor/dburles:factory";
 
 configure({ adapter: new Adapter() });
 
 if (Meteor.isClient) {
-    describe('EventInputForm', function testSuite() {
-        it('should render form properties correctly', function testDefaultFormLoading() {
-            const wrapper = mount(<EventInputForm />);
-            // Create test event for validity
-            expect(wrapper.find('select')).to.have.lengthOf(1);
-        })
+  describe("EventInputForm", function testSuite() {
+    it("Event input should be a string", function testDefaultFormLoading() {
+      const wrapper = mount(<EventInputForm />);
+      expect(wrapper.state.length, 3);
     });
+  });
 }
